488523505c1f4d011aaa1c61644454f2e25e9fbd
Artekka <arthur.ohumukini@gmail.com>
2025-05-20 19:55:41 -0700

    Added function to force CSS refresh

--- wp-content/themes/bloghash-child/functions.php
+++ wp-content/themes/bloghash-child/functions.php
@@ -4,13 +4,13 @@
  */
 
  // Load parent theme styles
-add_action( 'wp_enqueue_scripts', 'bloghash_child_enqueue_styles' );
-function bloghash_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
+ add_action( 'wp_enqueue_scripts', 'bloghash_child_enqueue_styles' );
+ function bloghash_child_enqueue_styles() {
+     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css') );
+     // Add versioning to child theme stylesheet to force refresh
+     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), filemtime(get_stylesheet_directory() . '/style.css') );
+ }
 
-
-}
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
